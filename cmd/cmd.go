7c2181ffd2d02abfd2d7e7a591826ee7901cad31
Palash Paul <61794570+PalashPaul-Ingka@users.noreply.github.com>
2022-11-17 09:02:07 +0100

    add gcp project id and secret (#1)

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/pal-paul/sa-key-rotation/pkg/gcloud/secret"
 	"github.com/pal-paul/sa-key-rotation/pkg/gcloud/serviceaccount"
 	"github.com/pal-paul/sa-key-rotation/pkg/github"
 )
@@ -13,19 +14,22 @@ var (
 	owner string
 	repo  string
 	token string
+
+	projectId              string
+	sa                     string
+	github_secret_key_name string
+	gcp_secret_key_name    string
 )
 
 func main() {
-	var (
-		sa          string
-		secret_name string
-	)
 	owner = os.Getenv("INPUT_OWNER")
 	repo = os.Getenv("INPUT_REPO")
 	token = os.Getenv("INPUT_TOKEN")
+	projectId = os.Getenv("INPUT_PROJECT_ID")
 
 	sa = os.Getenv("INPUT_SERVICE_ACCOUNT_EMAIL")
-	secret_name = os.Getenv("INPUT_SECRET_NAME")
+	github_secret_key_name = os.Getenv("INPUT_GITHUB_SECRET_KEY_NAME")
+	gcp_secret_key_name = os.Getenv("INPUT_GCP_SECRET_KEY_NAME")
 
 	client, err := serviceaccount.New(sa)
 	if err != nil {
@@ -38,7 +42,11 @@ func main() {
 		return
 	}
 	if len(keys) == 0 {
-		log.Fatal("no keys found for service account to rotate")
+		err = CreateRotate(client)
+		if err != nil {
+			log.Fatal("failed to rotate service account key: ", err)
+			return
+		}
 		return
 	}
 	if len(keys) > 1 {
@@ -46,24 +54,9 @@ func main() {
 		return
 	}
 	for _, key := range keys {
-		cred, err := client.CreateKey()
+		err = CreateRotate(client)
 		if err != nil {
-			log.Fatal("failed to create key: ", err)
-			return
-		}
-		gitClient, err := github.New(owner, repo, token)
-		if err != nil {
-			log.Fatal("failed to create github client: ", err)
-			return
-		}
-		secretData, err := json.Marshal(cred)
-		if err != nil {
-			log.Fatal("failed to marshal secret data: ", err)
-			return
-		}
-		err = gitClient.CreateUpdateRepoSecret(secret_name, string(secretData))
-		if err != nil {
-			log.Fatal("failed to create or update repo secret: ", err)
+			log.Fatal("failed to rotate service account key: ", err)
 			return
 		}
 		err = client.DeleteKey(key.Name)
@@ -72,4 +65,34 @@ func main() {
 			return
 		}
 	}
+}
+
+func CreateRotate(client serviceaccount.ServiceAccountInterface) error {
+	cred, err := client.CreateKey()
+	if err != nil {
+		return err
+	}
+	gitClient, err := github.New(owner, repo, token)
+	if err != nil {
+		return err
+	}
+	secretData, err := json.Marshal(cred)
+	if err != nil {
+		return err
+	}
+	err = gitClient.CreateUpdateRepoSecret(github_secret_key_name, string(secretData))
+	if err != nil {
+		return err
+	}
+	if gcp_secret_key_name != "" {
+		secretManager, err := secret.New(projectId)
+		if err != nil {
+			return err
+		}
+		if !secretManager.Exists(gcp_secret_key_name) {
+			secretManager.Create(gcp_secret_key_name)
+		}
+		secretManager.AddSecretVersion(gcp_secret_key_name, secretData)
+	}
+	return nil
 }
